1f3b13854dd9d09b4cc02b9c3b5f360c3ee6674d
newkayak12 <newkayak12@gmail.com>
2021-10-13 19:46:39 +0900

    facade

--- src/structuralPattern/facadePattern/Main.java
+++ src/structuralPattern/facadePattern/Main.java
@@ -1,11 +1,12 @@
 package structuralPattern.facadePattern;
 
+
 public class Main {
 	public static void main(String[] args) {
 		String tableName="Employee";
 		
 		//generating MySql HTML report and Oracle PDF report using Facade
-		HelperFacade.generateReport(HelpFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
-		HelperFacade.generateReport(HelpFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
+		HelpFacade.generateReport(HelpFacade.DBtype.MYSQL, HelpFacade.ReportType.HTML, tableName);
+		HelpFacade.generateReport(HelpFacade.DBtype.ORACLE, HelpFacade.ReportType.PDF, tableName);
 	}
 }
